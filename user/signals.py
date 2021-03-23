d4cc3b6a69b8c8448c4b6749a97bfcb48c6435ef
Ahmed Shoukr <ahmedshoukr10@gmail.com>
2021-03-22 18:07:24 -0700

    removed unnecssary imports and comments

--- user/signals.py
+++ user/signals.py
@@ -3,13 +3,11 @@ from django.dispatch import receiver
 from .models import DineSafelyUser, User_Profile
 from allauth.socialaccount.signals import pre_social_login
 from django.contrib.auth import get_user_model
-from django.http import HttpResponse
 from allauth.exceptions import ImmediateHttpResponse
 from django.contrib.auth.forms import AuthenticationForm
-from django.shortcuts import render
-from django.contrib.auth import login
 from django.contrib import messages
 from django.shortcuts import redirect
+
 
 @receiver(post_save, sender=DineSafelyUser)
 def create_profile(sender, instance, created, **kwargs):
@@ -25,12 +23,12 @@ def save_profile(sender, instance, **kwargs):
 @receiver(pre_social_login)
 def handle_duplicate_email(sender, request, sociallogin, **kwargs):
     try:
-        email = sociallogin.account.extra_data['email']
+        email = sociallogin.account.extra_data["email"]
     except:
-        email=None
+        email = None
     if get_user_model().objects.filter(email=email).exists():
         user = get_user_model().objects.get(email=email)
         if not list(user.socialaccount_set.all()):
             form = AuthenticationForm()
-            messages.error(request, 'Account already exists.')
-            raise ImmediateHttpResponse(redirect('/user/login'))
+            messages.error(request, "Account already exists.")
+            raise ImmediateHttpResponse(redirect("/user/login"))
